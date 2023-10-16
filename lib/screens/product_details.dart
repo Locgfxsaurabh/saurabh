c38fc30f06be81674bbf23e8a2429de0faebc4a6
potatopc <contact@locgfx.com>
2023-10-16 20:20:54 +0530

    push 3

--- lib/screens/product_details.dart
+++ lib/screens/product_details.dart
@@ -62,6 +62,7 @@ class _ProductDetailsState extends State<ProductDetails> {
           backgroundColor: Colors.white,
           elevation: 0,
           automaticallyImplyLeading: false,
+          centerTitle: false,
           title: Text(
             "Product details",
             style: TextStyle(color: Colors.black),
@@ -69,7 +70,8 @@ class _ProductDetailsState extends State<ProductDetails> {
           leadingWidth: 40,
           leading: Padding(
             padding: const EdgeInsets.only(left: 20),
-            child: InkWell(
+            child: GestureDetector(
+              behavior: HitTestBehavior.translucent,
               onTap: () {
                 Navigator.pop(context);
               },
